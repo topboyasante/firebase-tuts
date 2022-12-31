737740eab326168e8a9f16f5d4359a74f53ce903
Nana K <73991180+topboyasante@users.noreply.github.com>
2022-12-31 09:26:10 +0000

    99% Complete.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,41 @@ import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import AddTicket from './pages/AddTicket'
+import EditTicket from './pages/EditTicket'
 import Main from './pages/Main'
+import {AppContext} from './context/AppContext'
+
+import { db } from './firebase-config'
+import {collection,getDocs} from 'firebase/firestore'
 
 
 function App() {
 
+  const [users,setUsers] = useState([])
+  const usersCollectionRef = collection(db,"users")
+  useEffect(()=>{
+    async function getUsers(){
+      const data = await getDocs(usersCollectionRef)
+      setUsers(data.docs.map((doc)=>({...doc.data(), id:doc.id})))
+    }
+    getUsers()
+  })
 
   return (
-    <div className='bg-[#000000] h-screen text-white'>
+   <AppContext.Provider value={{users}}>
+     <div className='bg-[#000000] h-screen text-white'>
      
         <Navbar/>
         <section className='hidden md:block lg:block'>
             <Routes>
               <Route path='/' element={<Main/>}/>
               <Route path='add-ticket' element={<AddTicket/>}/>
+              <Route path=':ticketID' element={<EditTicket/>}/>
             </Routes>
         </section>
       <p className='md:hidden lg:hidden pt-[7vh] text-center text-xl'>Please View This on a Large Screen.</p>
     </div>
+   </AppContext.Provider>
   )
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { db } from '../firebase-config'
-import {collection,getDocs} from 'firebase/firestore'
+import React, { useContext } from 'react'
 import {BiEdit} from 'react-icons/bi'
 import {AiOutlineDelete} from 'react-icons/ai'
 import { Link } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { db } from '../firebase-config'
+import {doc,deleteDoc} from 'firebase/firestore'
 
 function Main() {
     
-  const [users,setUsers] = useState([])
-  const usersCollectionRef = collection(db,"users")
-  useEffect(()=>{
-    async function getUsers(){
-      const data = await getDocs(usersCollectionRef)
-      setUsers(data.docs.map((doc)=>({...doc.data(), id:doc.id})))
-    }
-    getUsers()
-  })
-
+  const {users} = useContext(AppContext)
+  async function deleteUser(id){
+    const userDoc = doc (db,"users", id)
+    await deleteDoc(userDoc)
+  }
   return (
        <section className='pt-[10vh] w-full lg:max-w-[1240px] mx-auto px-2 lg:px-5'>
            <section className='p-5'>
@@ -46,8 +42,13 @@ function Main() {
                                     <td className='text-center p-5 text-white'>{user.Category}</td>
                                     <td className='text-center p-5 text-white'>{user.Session}</td>
                                     <td className='flex items-center justify-around p-5'>
-                                        <BiEdit color='white' size={25} className='hover:scale-105 ease duration-200'/>
-                                        <AiOutlineDelete color='white' size={25} className='hover:scale-105 ease duration-200'/>
+                                       <Link to={`${user.id}`}>
+                                        <button>
+                                            <BiEdit color='white' size={25} className='hover:scale-105 ease duration-200'/>
+                                        </button>
+                                       </Link>
+                                        <AiOutlineDelete color='white' size={25} className='hover:scale-105 ease duration-200'
+                                        onClick={()=>{deleteUser(user.id)}}/>
                                     </td>
                                 </tr>
                             )
